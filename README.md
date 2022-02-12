c26f7a04a451f84d23321aed2c5b121adf9e9b9c
SirObby <70753342+SirObby@users.noreply.github.com>
2022-02-12 18:57:11 +0800

    README + Indent

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # My NeoVim Config
 
-Installation:
+## Installation:
 ```
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
@@ -11,3 +11,8 @@ Inside NeoVim:
 ```
 
 There are some dependencies, but idk them.
+
+## Problems
+
+For some reason Formatting doesn't work, as in I can't format my code for indenting and stuff.\
+So if you can do that somehow, you can Pull Request.
--- init.lua
+++ init.lua
@@ -103,7 +103,9 @@ vim.cmd([[
   set number
 
   set autoindent
-  "set auto-pairs
+  set tabstop=2
+  set shiftwidth=2
+  set softtabstop=2
 
   set guifont=LiberationMono\ NF:h9
 
@@ -220,18 +222,52 @@ require'nvim-treesitter.configs'.setup {
     }
 }
 
-require("null-ls").setup({
-    -- you can reuse a shared lspconfig on_attach callback here
-    on_attach = function(client)
-        if client.resolved_capabilities.document_formatting then
+_G.formatting = function(bufnr)
+    bufnr = tonumber(bufnr) or vim.api.nvim_get_current_buf()
+
+    vim.lsp.buf_request(
+        bufnr,
+        "textDocument/formatting",
+        { textDocument = { uri = vim.uri_from_bufnr(bufnr) } },
+        function(err, res)
+            if err then
+                local err_msg = type(err) == "string" and err or err.message
+                -- you can modify the log message / level (or ignore it completely)
+                vim.notify("formatting: " .. err_msg, vim.log.levels.WARN)
+                return
+            end
+
+            -- don't apply results if buffer is unloaded or has been modified
+            if not vim.api.nvim_buf_is_loaded(bufnr) or vim.api.nvim_buf_get_option(bufnr, "modified") then
+                return
+            end
+
+            if res then
+                vim.lsp.util.apply_text_edits(res, bufnr)
+                vim.api.nvim_buf_call(bufnr, function()
+                    vim.cmd("silent noautocmd update")
+                end)
+            end
+        end
+    )
+end
+
+local null_ls = require("null-ls")
+
+null_ls.setup({
+    -- add your sources / config options here
+    sources = ...,
+    on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+            -- wrap in an augroup to prevent duplicate autocmds
             vim.cmd([[
             augroup LspFormatting
                 autocmd! * <buffer>
-                autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()
+                autocmd BufWritePost <buffer> lua formatting(vim.fn.expand("<abuf>"))
             augroup END
             ]])
         end
-    end
+    end,
 })
 
 -- Mappings.
